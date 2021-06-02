c575d6553bc1c1178e111351106e33c6eb541ed8
Jesper Bengtsson Ekwall <jeppen@live.com>
2021-06-03 00:00:34 +0200

    Added Moonkin

--- src/components/ContentArea/MiniGames/Moonkin/Moonkin.js
+++ src/components/ContentArea/MiniGames/Moonkin/Moonkin.js
@@ -1,6 +1,7 @@
 import { BREAD_CRUMBS_COORDS } from "../../../../utilities/customfunctions";
 import "./base.scss";
 import BreadCrumb from "./BreadCrumb/BreadCrumb";
+import Moonkin_Standing_Straight from "../../../../resources/images/moonkin/mk_standing_straight.png";
 
 const Moonkin = () => {
   return (
@@ -9,6 +10,11 @@ const Moonkin = () => {
       {BREAD_CRUMBS_COORDS.map((crumbs, index) => (
         <BreadCrumb key={crumbs.x + "key" + index} crumbs={crumbs} />
       ))}
+      <img
+        className={"moonkin__standing__straight"}
+        src={Moonkin_Standing_Straight}
+        alt={"moonkin"}
+      />
     </>
   );
 };

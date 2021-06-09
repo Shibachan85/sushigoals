a73267e822910b286d75896c66027c47366ee45f
Jesper Bengtsson Ekwall <jeppen@live.com>
2021-06-09 21:25:29 +0200

    Added helmet settings

--- src/App.js
+++ src/App.js
@@ -2,12 +2,43 @@ import Main from "./components/Main/Main.js";
 import "./global/globalstyling.scss";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
+{
+  /* <meta
+property="og:title"
+content="A World of Warcraft classic commewnity"
+/>
+<meta
+property="og:site_name"
+content="A World of Warcraft classic commewnity"
+/>
+<meta property="og:type" content="website" />
+<meta
+property="og:image"
+content="%PUBLIC_URL%/sushi_guild_logo_394-264_.png"
+/>
+<meta property="og:url" content="https://www.sushiguild.eu" /> */
+}
+
 function App() {
   return (
     <HelmetProvider>
       <div className="App">
         <Helmet>
-          <title>Sushi: A world of warcraft commewnity - Classic</title>
+          {/* <meta
+            property="og:site_name"
+            content="A World of Warcraft classic commewnity"
+          />
+          <meta
+            property="og:title"
+            content="A World of Warcraft classic commewnity"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="%PUBLIC_URL%/sushi_guild_logo_394-264.png"
+          />
+          <meta property="og:url" content="https://www.sushiguild.eu" /> */}
         </Helmet>
         <Main />
       </div>
